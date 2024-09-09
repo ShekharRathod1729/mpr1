483048d0c2688aa9a8aa8f782e04902cf6c3c11d
ShekharRathod1729 <shekhar.rathod.1729@gmail.com>
2024-09-09 12:16:41 +0530

    Fixed a bug so that pressing enter in admin login also triggers clicking login button

--- admin-login.js
+++ admin-login.js
@@ -1,8 +1,16 @@
-function check(form) {
+var input = document.getElementById("enterPS");
+input.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("login").click();
+    }
+});
+
+function check(form) {    
     if(form.psw.value == "welcome1234") {
-        window.location.replace('admin.html')
+        window.location.replace('admin.html');
     }
     else {
-        alert("Incorrect password!")
+        alert("Incorrect password!");
     }
 }   
